83323767dedbfef9aad8f82069ba318706e0033b
23DP1AOrbi <166118293+23DP1AOrbi@users.noreply.github.com>
2024-09-03 11:01:23 +0000

    print_n_mainigie

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,7 +7,28 @@ public class App
 {
     public static void main( String[] args )
     {
-       System.out.println("yello");
+       System.out.println("Yello");
+       String name = "Juris";
+       int age = 20;
+       boolean isOnline = true;
+
+       // My code starts here
+
+       String name1 = "Laima";
+       int age1 = 17;
+
+
+       System.out.println(name1);
+       System.out.println(age1);
+       System.out.println(isOnline);
+
+       // Code ends
+
+
+       // Once Upon a Time
+       System.out.println("Once upon a time");
+       System.out.println("there was");
+       System.out.println("a program");
     }
 
 }
