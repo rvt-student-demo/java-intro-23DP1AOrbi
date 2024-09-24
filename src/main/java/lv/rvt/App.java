45eeacd772e88afa170e5d73f4a8f1433fb3ad93
23DP1AOrbi <166118293+23DP1AOrbi@users.noreply.github.com>
2024-09-24 05:57:46 +0000

    Gift tax

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -78,36 +78,59 @@ public class App
         // }
         
 
+        // Scanner scanner = new Scanner(System.in);
+
+        // System.out.println("Give points [0-100]:");
+        // int number = Integer.valueOf(scanner.nextLine());
+
+        // if (number < 0) {
+        //     System.out.println("Grade: impossible!");
+        // }
+        // else if ( 0 <= number  && number <= 49) {
+        //     System.out.println("Grade: failed");
+        // }
+        // else if (number >= 50 && number <= 59) {
+        //     System.out.println("Grade: 1");
+        // }
+        // else if (number >= 60 && number <= 69) {
+        //     System.out.println("Grade: 2");
+        // }
+        // else if (number >= 70 && number <= 79) {
+        //     System.out.println("Grade: 3");
+        // }
+        // else if (number >= 80 && number <= 89) {
+        //     System.out.println("Grade: 4");
+        // }
+        // else if (number >= 90 && number <= 100) {
+        //     System.out.println("Grade: 5");
+        // }
+        // else if (number > 100) {
+        //     System.out.println("Grade: incredible!");  
+        // }
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Give points [0-100]:");
+        System.out.println("Value of the gift?");
         int number = Integer.valueOf(scanner.nextLine());
 
-        if (number < 0) {
-            System.out.println("Grade: impossible!");
+        if (number < 5000) {
+            System.out.println("No tax!");
         }
-        else if ( 0 <= number  && number <= 49) {
-            System.out.println("Grade: failed");
+        else if (number >= 5000 && number <= 25000) {
+            System.out.println("Tax: " + (100 + (number - 5000) * 0.08));
         }
-        else if (number >= 50 && number <= 59) {
-            System.out.println("Grade: 1");
+        else if (number >= 25000 && number <= 55000) {
+            System.out.println("Tax: " + (1700 + (number - 25000) * 0.10));
         }
-        else if (number >= 60 && number <= 69) {
-            System.out.println("Grade: 2");
+        else if (number >= 55000 && number <= 200000) {
+            System.out.println("Tax: " + (4700 + (number - 55000) * 0.12));
         }
-        else if (number >= 70 && number <= 79) {
-            System.out.println("Grade: 3");
+        else if (number >= 200000 && number <= 1000000) {
+            System.out.println("Tax: " + (22100 + (number - 200000) * 0.15));
         }
-        else if (number >= 80 && number <= 89) {
-            System.out.println("Grade: 4");
+        else if (number > 1000000) {
+            System.out.println("Tax: " + (142100 + (number - 1000000) * 0.17));  
         }
-        else if (number >= 90 && number <= 100) {
-            System.out.println("Grade: 5");
-        }
-        else if (number > 100) {
-            System.out.println("Grade: incredible!");  
-        }
-
     }
 
 
