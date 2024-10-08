eb8aeea8f3a7accf048bffeb8dd8ea463af25e97
23DP1AOrbi <166118293+23DP1AOrbi@users.noreply.github.com>
2024-10-08 10:07:08 +0000

    Advanced astrology

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -16,86 +16,23 @@ public class App
         // or -> ||
         // not -> !
 
-        // Scanner scanner = new Scanner(System.in);
+    // printStars(5);
+    // printStars(3);
+    // printStars(9);
+    // System.out.println();
+    // printSquare(4);
+    // System.out.println();
+    // printRectangle(17, 3);
+    // System.out.println();
+    // printTriangle(4);
 
-        // System.out.println("Give numbers:");
-        // int sum = 0;
-        // int i = 0;
-        // int odd = 0;
+    // printTriangleTwo(4);
 
-        // while (true) {
-        //     int number = Integer.valueOf(scanner.nextLine());
-            
-        //     if (number == -1) {
-        //         break;
-        //     }
+    christmasTree(16);
 
-        //     if (number % 2 == 1) {
-        //         odd++;
-        //     }
-
-        //     sum += number;
-        //     i++;
-        // }
-        
-        // System.out.println("Thx! Bye!");
-        // System.out.println("Sum: " + sum);
-        // System.out.println("Numbers: " + i);
-        // System.out.println("Average: " + ( (double) sum/i));
-        // System.out.println("Even: " + (i-odd));
-        // System.out.println("Odd: " + odd);
-
-        // //////////////////
-
-    //     Scanner scanner = new Scanner(System.in);
-
-    //     System.out.println("How many times?");
-
-    //     int sk = Integer.valueOf(scanner.nextLine());
-        
-    //     for (int i = 0 ; i < sk ; i++ ) {
-    //         printText();
-    //     }
-    // }
-    // public static void printText() {
-    //     System.out.println("In a hole in the ground there lived a method");
-    // }
-
-    ///////////////////
-    // Scanner scanner = new Scanner(System.in);
-
-    // int sak = Integer.valueOf(scanner.nextLine());
-    // int beig = Integer.valueOf(scanner.nextLine());
-
-    //    divisibleByThreeInRange(sak ,  beig);
-    // }
-
-    // public static void divisibleByThreeInRange(int beginning, int end) {
-    //     for (int i = beginning ;  i <= end ; i++) {
-    //         if (i % 3 == 0) {
-    //             System.out.println(i);
-    //         }
-    //     }
-    // }
-    
-
-    printStars(5);
-    printStars(3);
-    printStars(9);
-
-    System.out.println();
-
-    printSquare(4);
-
-    System.out.println();
-
-    printRectangle(17, 3);
-
-    System.out.println();
-
-    printTriangle(4);
 
     }
+
 
     public static void printStars(int num) {
         for (int i = 1; i <= num ; i++) {
@@ -121,4 +58,38 @@ public class App
         }
     }
 
-}
+    ////////////
+
+    public static void printSpaces(int number) {
+        for (int i = 1; i <= number; i++) {
+                System.out.print(" ");
+        }
+    }
+
+    public static void printTriangleTwo(int size) {
+        for (int i = 1; i <= size; i++) {
+           printSpaces(size-i);
+           printStars(i); 
+        }
+        System.out.println();
+    }
+
+    public static void christmasTree(int height) {
+        printSpaces(height-1);
+            printStars(1);
+
+        for (int i = 2; i <= height; i++) {
+            printSpaces(height-i);
+            printStars(i+i-1);
+        }
+
+        for (int j = 1; j <= 2; j++) {
+            printSpaces(height-2);
+            printStars(3);
+        }
+    }
+
+
+    }
+
+
