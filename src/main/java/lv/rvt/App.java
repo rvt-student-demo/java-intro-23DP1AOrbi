93f85a4f9f2826cac2eddb3cdebbc0d17f52cb37
23DP1AOrbi <166118293+23DP1AOrbi@users.noreply.github.com>
2024-10-22 07:46:01 +0000

    Print in range

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -14,18 +14,6 @@ public class App
         // or -> ||
         // not -> !
 
-        // ArrayList<String> nameList = new ArrayList<>();
-        // Scanner scanner = new Scanner(System.in);
-
-        // while (true) {
-        //     String name = scanner.nextLine();
-        //     if (name == "") {
-        //         System.out.println(nameList.get(2));
-        //         break;
-        //     }
-        //     nameList.add(name);
-        // }
-
         // // Pievieno elementus
         // intList.add(1);
         // // Saņemt elementu
@@ -35,40 +23,66 @@ public class App
 
 
 
-        Scanner scan = new Scanner(System.in);
-        ArrayList<Integer> intNum = new ArrayList<>();
+        // Scanner scan = new Scanner(System.in);
+        // ArrayList<Integer> intNum = new ArrayList<>();
 
-        while (true) {
-            int number = Integer.valueOf(scan.nextLine());
-            if (number == -1) {
-                break;
-            }
-            intNum.add(number);
-        }
+        // while (true) {
+        //     int number = Integer.valueOf(scan.nextLine());
+        //     if (number == -1) {
+        //         break;
+        //     }
+        //     intNum.add(number);
+        // }
 
-        System.out.print("From where? ");
-        int begin = Integer.valueOf(scan.nextLine());
+        // System.out.print("From where? ");
+        // int begin = Integer.valueOf(scan.nextLine());
 
-        while (begin >= intNum.size()) {
-            System.out.println("Index needs to be within range");
-            int start = Integer.valueOf(scan.nextLine());
-            begin = start;
-        }
+        // while (begin >= intNum.size() || begin < 0) {
+        //     System.out.println("Index needs to be within range");
+        //     int start = Integer.valueOf(scan.nextLine());
+        //     begin = start;
+        // }
 
-        System.out.print("To where? ");
-        int end = Integer.valueOf(scan.nextLine());
+        // System.out.print("To where? ");
+        // int end = Integer.valueOf(scan.nextLine());
         
-        while (end >= intNum.size()) {
-            System.out.println("Index needs to be within range");
-            int fin = Integer.valueOf(scan.nextLine());
-            end = fin;
+        // while (end >= intNum.size() || begin < 0) {
+        //     System.out.println("Index needs to be within range");
+        //     int fin = Integer.valueOf(scan.nextLine());
+        //     end = fin;
+        // }
+        // System.out.println();
+
+        // for (int i = begin ; i <= end ; i++) {
+        //         System.out.println(intNum.get(i));
+        //     }
+
+        
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+            numbers.add(3);
+            numbers.add(2);
+            numbers.add(6);
+            numbers.add(-1);
+            numbers.add(5);
+            numbers.add(1);
+
+            System.out.println("The numbers in the range [0, 5]");
+            printNumbersInRange(numbers, 0, 5);
+
+            System.out.println("The numbers in the range [3, 10]");
+            printNumbersInRange(numbers, 3, 10);
+        
+
         }
-        System.out.println();
 
-        for (int i = begin ; i <= end ; i++) {
-                System.out.println(intNum.get(i));
+        public static void printNumbersInRange(ArrayList<Integer> numbers, int lowerLimit, int upperLimit) {
+
+            for (int x : numbers) {
+                if (x >= lowerLimit && x <= upperLimit) {
+                    System.out.println(x);
+                }
             }
-
         }
 
     }
