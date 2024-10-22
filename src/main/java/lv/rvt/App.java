9f1a66cd850be639f7e672a3e138a317e7b0a157
23DP1AOrbi <166118293+23DP1AOrbi@users.noreply.github.com>
2024-10-22 06:15:58 +0000

    Only these numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -33,107 +33,29 @@ public class App
         // // Cik ir elementu masīvā?
         // intList.size();
 
-        // Scanner scanner = new Scanner(System.in);
 
-        // int userInput = Integer.valueOf(scanner.nextLine());
 
-        // ArrayList<Integer> intList = new ArrayList<>();
-
-        // for (int i = 0; i < intList.size(); i++) {
-        //     System.out.println(
-        //         intList.get(i)
-        //     );
-        // }
-
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> intSum = new ArrayList<>();
-
-        int sum = 0;
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Integer> intNum = new ArrayList<>();
 
         while (true) {
-            int number = Integer.valueOf(scanner.nextLine());
-
-            if (number == 0) {
-                // System.out.println(sum);
-                sum = 0;
-
-                for (int i = 0; i < intSum.size(); i++){
-                    sum += intSum.get(i);
-                }
-                // System.out.println(intSum.get(number));
-                System.out.println(sum);
+            int number = Integer.valueOf(scan.nextLine());
+            if (number == -1) {
                 break;
             }
-
-            intSum.add(number);
-            // sum += number;
+            intNum.add(number);
         }
+        System.out.print("From where? ");
+        int begin = Integer.valueOf(scan.nextLine());
 
+        System.out.print("To where? ");
+        int end = Integer.valueOf(scan.nextLine());
 
-
-        
- 
-        
-
-
-
-
-
-
-
-    }
-
-
-    // public static void printStars(int num) {
-    //     for (int i = 1; i <= num ; i++) {
-    //         System.out.print("*");
-    //     }
-    //     System.out.println();
-    // }
-
-    // public static void printSquare(int size) {
-    //     for (int i = 1 ; i <= size; i++) {
-    //         printStars(size);
-    //     }
-
-    // }
-    // public static void printRectangle(int width, int height) {
-    //     for (int i = 1; i <= height; i++) {
-    //         printStars(width);
-    //     }
-    // }
-    // public static void printTriangle(int size) {
-    //     for (int i = 1; i <= size ; i++) {
-    //         printStars(i);
-    //     }
-    // }
-
-    // public static void printSpaces(int number) {
-    //     for (int i = 1; i <= number; i++) {
-    //             System.out.print(" ");
-    //     }
-    // }
-
-    // public static void printTriangleTwo(int size) {
-    //     for (int i = 1; i <= size; i++) {
-    //        printSpaces(size-i);
-    //        printStars(i); 
-    //     }
-    //     System.out.println();
-    // }
-
-    // public static void christmasTree(int height) {
-    //     for (int i = 1; i <= height; i++) {
-    //         printSpaces(height-i);
-    //         printStars(i+i-1);
-    //     }
-
-    //     for (int j = 1; j <= 2; j++) {
-    //         printSpaces(height-2);
-    //         printStars(3);
-    //     }
-    // }
-
+        for (int i = begin ; i <= end ; i++) {
+                System.out.println(intNum.get(i));
+            }
+            
+        }
 
     }
 
