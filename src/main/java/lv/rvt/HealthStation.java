d0c995dfddec3439d291a64d210a9fe10538a189
23DP1AOrbi <166118293+23DP1AOrbi@users.noreply.github.com>
2024-12-10 08:50:48 +0000

    Health station (Part 3 - Counting weighings)

--- src/main/java/lv/rvt/HealthStation.java
+++ src/main/java/lv/rvt/HealthStation.java
@@ -1,11 +1,17 @@
 package lv.rvt;
 
 public class HealthStation {
+    int weighCount;
     public int weigh(Person person) {
+        weighCount++;
         return person.getWeight();
+
     }
     public void feed(Person person) {
         person.setWeight(person.getWeight()+1);
+    }
+    public int weighings() {
+        return weighCount;
     }
 
 }
