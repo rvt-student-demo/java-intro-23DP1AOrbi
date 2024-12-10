4607307c12ed58c3e22d521a82a3d03ba9716b5c
23DP1AOrbi <166118293+23DP1AOrbi@users.noreply.github.com>
2024-12-10 11:19:34 +0000

    Card payments (Part 2 - Payment terminal and cash)

---
+++ src/main/java/lv/rvt/PaymentTerminal.java
@@ -0,0 +1,35 @@
+package lv.rvt;
+
+public class PaymentTerminal {
+    private double money;  // amount of cash
+    private int affordableMeals; // number of sold affordable meals
+    private int heartyMeals;  // number of sold hearty meals
+
+    public PaymentTerminal() {
+       this.money = 1000;
+    }
+
+    public double eatAffordably(double payment) {
+        affordableMeals++;
+        this.money += 2.5;
+        return payment-2.5;
+        }
+        // an affordable meal costs 2.50 euros
+        // increase the amount of cash by the price of an affordable meal and return the change
+        // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
+
+
+    public double eatHeartily(double payment) {
+        heartyMeals++;
+        this.money += 4.3;
+        return payment-4.3;
+
+        // a hearty meal costs 4.30 euros
+        // increase the amount of cash by the price of a hearty meal and return the change
+        // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
+    }
+
+    public String toString() {
+        return "money: " + money + ", number of sold afforable meals: " + affordableMeals + ", number of sold hearty meals: " + heartyMeals;
+    }
+}
