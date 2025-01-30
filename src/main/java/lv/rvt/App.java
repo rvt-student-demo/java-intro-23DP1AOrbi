afe79a0d46b1bfa973277bea18540f2cfbdf8d04
23DP1AOrbi <166118293+23DP1AOrbi@users.noreply.github.com>
2025-01-30 12:13:26 +0000

    Warehouse Part 1 & 2

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,37 +8,28 @@ import lv.rvt.secondSem.Box;
 public class App {
     public static void main( String[] args ) throws Exception {
 
-        ArrayList<Person> persons = new ArrayList<Person>();
-        persons.add(new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200));
-        persons.add(new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028"));
+        ProductWarehouse juice = new ProductWarehouse("Juice", 1000.0);
+        juice.addToWarehouse(1000.0);
+        juice.takeFromWarehouse(11.3);
+        System.out.println(juice.getName()); // Juice
+        juice.addToWarehouse(1.0);
+        System.out.println(juice);  
 
-        printPersons(persons);
+    //     ArrayList<Person> persons = new ArrayList<Person>();
+    //     persons.add(new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200));
+    //     persons.add(new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028"));
 
-    } 
-    public static void printPersons(ArrayList<Person> list) {
-        for (Person person : list) {
-            System.out.println(person);
+    //     printPersons(persons);
+
+    // } 
+    // public static void printPersons(ArrayList<Person> list) {
+    //     for (Person person : list) {
+    //         System.out.println(person);
             
-        }
-    }
+    //     }
+    // }
         
         
-        
-        
-                // Teacher ada = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200);
-                // Teacher esko = new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
-                // System.out.println(ada);
-                // System.out.println(esko);
-                
-                // Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
-                
-                // int i = 0;
-                // while (i < 25) {
-                //   ollie.study();
-                //   i = i + 1;
-                // }
-                // System.out.println(ollie);
-                
                 //////////////////////////
         
                 /// comma seperated values
@@ -58,9 +49,6 @@ public class App {
                 // // cik elementu masīvā? (priekš parasta masīva)
         
                 }
-        
+
+            }
     
-
-
-
-
